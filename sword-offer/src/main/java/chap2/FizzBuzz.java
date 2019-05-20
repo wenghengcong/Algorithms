2f5c2d68880ebf42a20f021c154f4678ea112e57
wenghengcong <wenghengcong@icloud.com>
2019-05-20 23:03:44 +0800

    no message

--- sword-offer/src/main/java/chap2/FizzBuzz.java
+++ sword-offer/src/main/java/chap2/FizzBuzz.java
@@ -19,21 +19,23 @@ import java.util.Map;
 * */
 public class FizzBuzz {
 
+    /**
+     * @param n: An integer
+     * @return: A list of strings.
+     */
     public static List<String> fizzBuzz(int n) {
+        // write your code here
         List<String> result = new ArrayList();
         Map fizzMap = new HashMap();
         Map buzzMap = new HashMap();
         fizzMap.put(3, "fizz ");
         buzzMap.put(5, " buzz");
-        for (int i=0; i<=n;i++) {
-            String mapFizz = (String) fizzMap.get(i%3+3);
-            String mapBuzz = (String) buzzMap.get(i%5+5);
-            String nStr = mapFizz + " " + mapBuzz;
-            // 1.fizz为空，即null_buzz
-            // 2.buzz为空，即fizz_null
-            // 3.fizz__buzz
+        for (int i=1;i <= n ;i++ ) {
+            String mapFizz = (String)fizzMap.get(i%3+3);
+            String mapBuzz = (String)buzzMap.get(i%5+5);
+            String nStr = mapFizz + mapBuzz;
             nStr = nStr.replaceFirst(" ", "");
-            nStr = nStr.replaceAll("null ", "");
+            nStr = nStr.replaceAll("null", "");
             if (nStr.length() == 0) {
                 nStr = String.valueOf(i);
             }
