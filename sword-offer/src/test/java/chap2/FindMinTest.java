289cc653bb6012b62e2a0fe1c0d1086978d1e09b
wenghengcong <wenghengcong@icloud.com>
2019-05-26 20:38:18 +0800

    寻找最小数

---
+++ sword-offer/src/test/java/chap2/FindMinTest.java
@@ -0,0 +1,25 @@
+package chap2;
+
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+/**
+ * @author wenghengcong
+ * @className: FindMinTest
+ * @description:
+ * @date 2019-05-2620:34
+ */
+public class FindMinTest {
+
+    private static final Logger log = LogManager.getLogger(TestChap2.class.getName());
+
+    @Test
+    public void findMin() {
+        int[] testarr = new int[]{4,5,6,7,1,2};
+        int min = FindMin.findMin(testarr);
+        log.info(min);
+    }
+}
